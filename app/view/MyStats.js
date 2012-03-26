8187b7b0d76f359367614dcefdcd8a27d02a01e1
Erik Töyrä <erik.toyra@gmail.com>
2012-03-27 00:05:56 +0200

    Added dummys for personal statistics.

--- app/view/MyStats.js
+++ app/view/MyStats.js
@@ -3,6 +3,11 @@ Ext.define('EM.view.MyStats', {
 	xtype: 'mystats',
 	
 	config: {
-		id: 'my-stats'
+		id: 'my-stats',
+		
+		html: [
+			'<div id="league-select">Alla ligor</div>',
+			'<div id="my-ranking">47<span class="total-contestants">/241</span></div>'
+		].join("")
 	}
 })
