f038299cc0e4d6160a0f6f6a1064303b4630a00b
Erik Töyrä <erik.toyra@gmail.com>
2012-05-01 09:30:16 +0200

    Added EM-Tipset logo in top bar.

--- app/view/TopToolbar.js
+++ app/view/TopToolbar.js
@@ -12,6 +12,14 @@ Ext.define('EM.view.TopToolbar', {
 		items: [
 		{
 			xtype: 'container',
+			docked: 'left',
+
+			html: [
+				'<img id="em-tipset-logo" src="resources/images/em-tipset-logo.png" />'
+			]
+		},
+		{
+			xtype: 'container',
 			docked: 'right',
 				
 			html: [
